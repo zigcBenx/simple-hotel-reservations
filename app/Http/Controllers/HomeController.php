60db54b740c22411df1b076988cce9a51680b4cf
zigcBenx <zigac.benko@gmail.com>
2021-08-18 22:01:36 +0200

    WIP[TASK2] Implement contact form validation on backend with custom request

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreReservationRequest;
 use App\Models\Room;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,16 @@ class HomeController extends Controller
     public function index()
     {
         $rooms = Room::all();
-        return view('welcome', compact('rooms'));
+        $data = 'Nc';
+        return view('welcome', compact('rooms', 'data'));
+    }
+
+    public function store(StoreReservationRequest $request)
+    {
+        $data = $request->validated();
+        // do something with data here
+
+        $rooms = Room::all();
+        return view('welcome', compact('rooms', 'data'));
     }
 }
--- resources/views/contact_form.blade.php
+++ resources/views/contact_form.blade.php
@@ -1,56 +1,62 @@
 <div class="container card">
-    <form class="card-body">
+    <form method="POST" action="{{route('store-test')}}" enctype="multipart/form-data" class="card-body">
+        @csrf
         <h4 class="text-center">Make reservation</h4>
         <div class="form-group row mb-3">
             <div class="col">
                 <label for="date-picker-from">From:</label>
                 <div class="input-group">
                     <div class="input-group-text"><i class="far fa-calendar"></i></div>
-                    <input type="text" class="form-control" id="date-picker-from" required>
-
+                    <input type="text" class="form-control @if($errors->has('date-picker-from')) is-invalid @endif" name="date-picker-from" id="date-picker-from" value="{{old('date-picker-from')}}">
                 </div>
+                @error('date-picker-from')<p class="text-danger">*{{$message}}</p>@enderror
             </div>
             <div class="col">
                 <label for="date-picker-to">To:</label>
                 <div class="input-group">
                     <div class="input-group-text"><i class="far fa-calendar"></i></div>
-                    <input type="text" class="form-control" id="date-picker-to" required>
+                    <input type="text" class="form-control @if($errors->has('date-picker-to')) is-invalid @endif" name="date-picker-to" id="date-picker-to" value="{{old('date-picker-to')}}">
                 </div>
+                @error('date-picker-to')<p class="text-danger">*{{$message}}<p>@enderror
             </div>
         </div>
 
         <div class="form-group mb-3">
-            <select class="form-select" aria-label="Choose your room">
+            <select class="form-select" aria-label="Choose your room" name="room" id="room">
                 @foreach($rooms as $room)
                     <option value="{{$room->id}}">{{ $room->name }}</option>
                 @endforeach
             </select>
+            @error('room')<p class="text-danger">*{{$message}}</p>@enderror
         </div>
 
         <hr>
 
         <div class="form-group mb-3">
             <label for="name">Name and surname:</label>
-            <input type="text" id="name" class="form-control" placeholder="John Smith" required>
+            <input type="text" name="name" id="name" class="form-control @if($errors->has('name')) is-invalid @endif" placeholder="John Smith" value="{{old('name')}}">
+            @error('name')<p class="text-danger">*{{$message}}</p>@enderror
         </div>
 
         <div class="form-group mb-3">
             <div class="row">
                 <div class="col">
                     <label for="mail">E-mail:</label>
-                    <input type="text" id="mail" class="form-control" placeholder="john@smith.com" required>
+                    <input type="text" name="email" id="email" class="form-control @if($errors->has('email')) is-invalid @endif" placeholder="john@smith.com" value="{{old('email')}}">
+                    @error('email')<p class="text-danger">*{{$message}}</p>@enderror
                 </div>
                 <div class="col">
                     <label for="phone">Phone:</label>
-                    <input type="text" id="phone" class="form-control" placeholder="(+386) 31 200 300" required>
+                    <input type="text" name="phone" id="phone" class="form-control @if($errors->has('phone')) is-invalid @endif" placeholder="(+386) 31 200 300" value="{{old('phone')}}">
+                    @error('phone')<p class="text-danger">*{{$message}}</p>@enderror
                 </div>
             </div>
         </div>
 
         <div class="form-group mb-3">
             <div class="form-floating">
-                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-                <label for="floatingTextarea2">Additional information</label>
+                <textarea class="form-control" placeholder="Leave a comment here" name="info" id="info" style="height: 100px"></textarea>
+                <label for="info">Additional information</label>
             </div>
         </div>
 
